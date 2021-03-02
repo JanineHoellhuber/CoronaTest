223024660e366581687057faa42785f3ffc04107
janine <janine.hoellhuber@gmx.at>
2021-03-02 09:40:45 +0100

    StatisticController

--- Test.Web/Controllers/StatisticsController.cs
+++ Test.Web/Controllers/StatisticsController.cs
@@ -30,6 +30,7 @@ namespace Test.Web.Controllers
         /// <response code="200">Die Abfrage war erfolgreich.</response>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<StatisticsDto>> GetStatisticsInPeriode(DateTime from, DateTime to)
         {
             var examinations = await _unitOfWork.ExaminationRepository.GetExaminationsByFilterAsync(from, to);
@@ -38,13 +39,45 @@ namespace Test.Web.Controllers
 
             if (examinations != null)
             {
-                statistic.CountExaminations = examinations.Count();
-                statistic.CountUnknownTest = examinations.Count(u => u.TestResult == TestResult.Unknown);
-                statistic.CountPositiveTest = examinations.Count(p => p.TestResult == TestResult.Positive);
-                statistic.CountNegativeTest = examinations.Count(n => n.TestResult == TestResult.Negative);
+                return BadRequest();
             }
+
+            statistic.CountExaminations = examinations.Count();
+            statistic.CountUnknownTest = examinations.Count(u => u.TestResult == TestResult.Unknown);
+            statistic.CountPositiveTest = examinations.Count(p => p.TestResult == TestResult.Positive);
+            statistic.CountNegativeTest = examinations.Count(n => n.TestResult == TestResult.Negative);
 
             return Ok(statistic);
         }
+
+        /// <summary>
+        /// Liefert die Teststatistik in der Gemeinde und im Zeitraum
+        /// </summary>
+        /// <response code="200">Die Abfrage war erfolgreich.</response>
+        [HttpGet]
+        [Route("byPostalCode")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<StatisticsDto>> GetStatisticsInAreaAndPeriode(string postalCode, DateTime from, DateTime to)
+        {
+            var examinations = await _unitOfWork.ExaminationRepository.GetExaminationsWithFilterByPostCodeAndDateAsync(postalCode, from, to);
+
+            StatisticsDto statistic = new StatisticsDto();
+
+            if (examinations != null)
+            {
+
+                return BadRequest();
+
+            }
+
+            statistic.CountExaminations = examinations.Count();
+            statistic.CountUnknownTest = examinations.Count(u => u.TestResult == TestResult.Unknown);
+            statistic.CountPositiveTest = examinations.Count(p => p.TestResult == TestResult.Positive);
+            statistic.CountNegativeTest = examinations.Count(n => n.TestResult == TestResult.Negative);
+
+            return Ok(statistic);
+        }
+
     }
 }
